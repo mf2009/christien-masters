c26ad250eb9056f13d0ac3bbf4aacc7edf3ad853
pypmannetjies <pypmannetjies@c1cd582d-6cd9-6606-c5f1-7323edcfcf60>
2013-03-06 14:25:08 +0000

    Added ContinuousData and bounds (the screen size) Both are spewed as well

--- com/pypmannetjies/pointerlocation/MyPointerLocationView.java
+++ com/pypmannetjies/pointerlocation/MyPointerLocationView.java
@@ -1,12 +1,16 @@
 package com.pypmannetjies.pointerlocation;
 
 import com.pypmannetjies.pointerlocation.AwesomeGestureListener.GestureType;
+import com.pypmannetjies.pointerlocation.GestureData.ScreenOrientation;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Canvas;
-import android.util.Log;
+import android.graphics.Point;
+import android.view.Display;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.WindowManager;
 
 public class MyPointerLocationView extends com.android.internal.widget.PointerLocationView {
 	
@@ -19,22 +23,42 @@ public class MyPointerLocationView extends com.android.internal.widget.PointerLo
 	public MyPointerLocationView(Context c) {
 		super(c);
 		this.theContext = c;
-		//DataRecorder.openFile("touch_data" + System.currentTimeMillis() + ".csv", c);
-		DataSpewer.spewHeader();
-		endOfLastGestureTime = -1;
 		
+		writeHeadersAndBounds();
+		
+		endOfLastGestureTime = -1;
 		awesome = new AwesomeGestureListener();
 		detector = new GestureDetector(awesome);
 	}
 
+	private void writeHeadersAndBounds() {
+		DataSpewer.spewString(GestureData.getHeader());		
+		DataSpewer.spewString(ContinuousData.getHeader());
+		ContinuousData cd = new ContinuousData();
+		cd.setBounds(getScreenSize());
+		DataSpewer.spewString("BOUNDS," + cd.toString());
+	}
+	
 	@Override
 	public void addTouchEvent(MotionEvent event) {
 		detector.onTouchEvent(event);
 		
+		// Orientation + keyboard
+		int intOrientation = getResources().getConfiguration().orientation;
+        int intKeyboardHidden = getResources().getConfiguration().keyboardHidden;
+        ScreenOrientation screenOrientation = (intOrientation == Configuration.ORIENTATION_PORTRAIT) ? ScreenOrientation.PORTRAIT :  ScreenOrientation.LANDSCAPE;
+        boolean keyboardVisible = (intKeyboardHidden == Configuration.KEYBOARDHIDDEN_YES) ? false : true;
+		
+		// Continuous data
+		ContinuousData continuousData = new ContinuousData();		
+		String s = continuousData.setData(event, awesome.getLastGestureType(), screenOrientation, keyboardVisible);
+		DataSpewer.spewString(s);
+		
+		// Gestures
 		int action = event.getAction(); 
 
 		if (action == MotionEvent.ACTION_DOWN) {
-			gestureData = new GestureData(GestureType.UNKNOWN, 0); 
+			gestureData = new GestureData(GestureType.UNKNOWN, -1); 
 			gestureData.recordStartTime();
 			gestureData.addMotionData(event);		
 			
@@ -46,10 +70,10 @@ public class MyPointerLocationView extends com.android.internal.widget.PointerLo
 			gestureData.setType(awesome.getLastGestureType());			
 			gestureData.recordEndTime();
 			gestureData.addMotionData(event);
-			gestureData.setFinalData(event, getResources().getConfiguration().orientation, getInterstrokeTime());
+			gestureData.setFinalData(event, getInterstrokeTime(), screenOrientation, keyboardVisible);
 			
 			//DataRecorder.addToFile(gestureData);
-			DataSpewer.spewAverages(gestureData);
+			DataSpewer.spewGesture(gestureData);
 		}		
 		
 		super.addTouchEvent(event);
@@ -70,6 +94,18 @@ public class MyPointerLocationView extends com.android.internal.widget.PointerLo
 		endOfLastGestureTime = System.currentTimeMillis();
 		
 		return interstrokeTime;
+	}
+	
+	/**
+	 * Find the screen size
+	 * @return The screen size
+	 */
+	private Point getScreenSize() {
+		WindowManager wm = (WindowManager) theContext.getSystemService(Context.WINDOW_SERVICE);
+		Display display = wm.getDefaultDisplay();
+		Point size = new Point();
+		size.set(display.getWidth(), display.getHeight());
+		return size;
 	}
 	
 	@Override
